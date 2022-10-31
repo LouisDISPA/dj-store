0f9263934c0361fa3b6ff489bf5ca7d2c3d47453
Louis Dispa <louis.dispa@insa-rouen.fr>
2022-10-31 16:06:31 +0100

    Add login page

---
+++ ui/src/lib/TextInput.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let label: string | undefined = undefined;
+	export let placeholder: string | undefined = label ? label.toLowerCase() + '...' : undefined;
+</script>
+
+<div class="form-control">
+	{#if label}
+		<div class="label">
+			<span class="label-text">{label}</span>
+		</div>
+	{/if}
+	<input type="text" {placeholder} class="input input-bordered" />
+</div>
---
+++ ui/src/routes/login/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Button from '$lib/Button.svelte';
+	import TextInput from '$lib/TextInput.svelte';
+</script>
+
+<div class="hero">
+	<div class="hero-content flex-col lg:flex-row-reverse">
+		<div class="text-center lg:text-left my-3">
+			<h1 class="text-2xl font-bold">Login to your account</h1>
+		</div>
+		<TextInput label="Username" />
+		<TextInput label="Password" />
+		<div class="mt-5">
+			<Button label="Connect" />
+		</div>
+	</div>
+</div>
