6afc85a00492b343c51388d5a19f2ae526174f87
Dispa Louis <louis.dispa@insa-rouen.fr>
2023-01-23 17:46:20 +0100

    Fix scanner fps

---
+++ ui/src/lib/Scanner.svelte
@@ -0,0 +1,140 @@
+<script lang="ts">
+	import { onDestroy } from 'svelte';
+	import { goto } from './utils.js';
+	import ScannerBorders from './ScannerBorders.svelte';
+	import jsQR from 'jsqr';
+
+	let video: HTMLVideoElement;
+	let canvas: HTMLCanvasElement;
+	let active = true;
+	let stream: MediaStream | null = null;
+
+	onDestroy(stopMediaStream);
+
+	const isMediaStream = (
+		candidate: MediaStream | MediaSource | Blob | null
+	): candidate is MediaStream => candidate !== null && 'getTracks' in candidate;
+
+	function startMediaStream() {
+		navigator.mediaDevices
+			.getUserMedia({
+				audio: false,
+				video: {
+					facingMode: 'environment'
+				}
+			})
+			.then((userStream) => {
+				stream = userStream;
+			})
+			.catch((err) => {
+				console.log(err);
+				alert('Please allow camera access to scan QR code');
+				goto('/');
+			});
+	}
+
+	function stopMediaStream() {
+		console.log('stopping media stream');
+		if (isMediaStream(stream)) {
+			stream.getTracks().forEach((track) => {
+				track.stop();
+				stream?.removeTrack(track);
+			});
+		}
+		video.srcObject = null;
+	}
+
+	const startCapturing = (): void => {
+		if (!canvas || !video) return;
+		const context = canvas.getContext('2d');
+		if (!context) return;
+		const { width, height } = canvas;
+		context.drawImage(video, 0, 0, width, height);
+		const imageData = context.getImageData(0, 0, width, height);
+		const qrCode = jsQR(imageData.data, width, height);
+		if (qrCode === null) {
+			setTimeout(startCapturing, 750);
+		} else {
+			video.pause();
+			const data = qrCode.data;
+			console.log(data);
+
+			const currentUrl = window.location.origin;
+			if (!data.startsWith(currentUrl)) {
+				alert('Invalid QR code: please scan a QR code for this app.');
+				video.play();
+				setTimeout(startCapturing, 750);
+				return;
+			} else {
+				goto(data);
+			}
+		}
+	};
+	const handleCanPlay = (): void => {
+		console.log('canplay');
+		if (canvas === null || canvas === null || video === null || video === null) {
+			return;
+		}
+		canvas.width = video.videoWidth;
+		canvas.height = video.videoHeight;
+		startCapturing();
+	};
+	$: if (video !== null && stream) {
+		console.log('Resolve, stream');
+		video.srcObject = stream;
+		video.play().catch(console.error);
+	}
+	$: if (active && startMediaStream) {
+		startMediaStream();
+	}
+</script>
+
+<div class={`scanner ${active ? '' : 'scanner--hidden'}`}>
+	<div class="scanner__aspect-ratio-container">
+		<canvas bind:this={canvas} class="scanner__canvas" />
+		<!-- svelte-ignore a11y-media-has-caption -->
+		<video bind:this={video} on:canplay={handleCanPlay} class="scanner__video">
+			<!-- <track kind="captions" /> -->
+		</video>
+		<ScannerBorders />
+	</div>
+
+	<div class="scanner-tip">
+		<div>Scan a QR code with your camera to join a room.</div>
+	</div>
+</div>
+
+<style>
+	.scanner {
+		width: 100%;
+		max-width: 500px;
+	}
+	.scanner--hidden {
+		display: none;
+	}
+	.scanner__aspect-ratio-container {
+		position: relative;
+		overflow: hidden;
+		padding-bottom: 100%;
+		border-radius: 10%;
+	}
+	.scanner__video {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		border-radius: inherit;
+		outline: none;
+		object-fit: cover;
+	}
+	.scanner__canvas {
+		display: none;
+	}
+	.scanner-tip {
+		display: flex;
+		justify-content: center;
+		margin-top: 15px;
+		font-size: 0.8rem;
+	}
+</style>
--- ui/src/routes/scan/+page.svelte
+++ ui/src/routes/scan/+page.svelte
@@ -2,78 +2,7 @@
 	import { goto } from '$app/navigation';
 	import Button from '$lib/Button.svelte';
 	import Hero from '$lib/Hero.svelte';
-	import Spinner from '$lib/Spinner.svelte';
-	import { onDestroy, onMount } from 'svelte';
-
-	let stream: undefined | MediaStream;
-	let loading = true;
-	let stopped = false;
-	let canvasElement: HTMLCanvasElement;
-
-	const stopStream = () => {
-		stopped = true;
-		if (stream) {
-			stream.getTracks().forEach((track) => track.stop());
-		}
-	};
-
-	onDestroy(stopStream);
-
-	onMount(async function () {
-		const jsQR = (await import('jsqr')).default;
-		const video = document.createElement('video');
-		const canvas = canvasElement?.getContext('2d') as CanvasRenderingContext2D;
-
-		if (canvas == null) {
-			alert('Error loading qrcode scanner.');
-			goto('/');
-		}
-
-		// Use facingMode: environment to attemt to get the front camera on phones
-		navigator.mediaDevices
-			.getUserMedia({ video: { facingMode: 'environment' } })
-			.then(function (mediaStream) {
-				video.srcObject = mediaStream;
-				video.setAttribute('playsinline', 'true'); // required to tell iOS safari we don't want fullscreen
-				video.play();
-				stream = mediaStream;
-				requestAnimationFrame(tick);
-			})
-			.catch(function () {
-				loading = false;
-				alert('Failed to get camera access.');
-				goto('/');
-			});
-
-		function tick() {
-			if (video.readyState === video.HAVE_ENOUGH_DATA) {
-				loading = false;
-				canvasElement.hidden = false;
-				canvasElement.height = video.videoHeight;
-				canvasElement.width = video.videoWidth;
-				canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-				var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
-				var code = jsQR(imageData.data, imageData.width, imageData.height, {
-					inversionAttempts: 'dontInvert'
-				});
-				if (code) checkQrCode(code);
-			}
-			if (!stopped) {
-				requestAnimationFrame(tick);
-			}
-		}
-	});
-
-	function checkQrCode(code: { data: string }) {
-		if (!code.data.startsWith('http')) {
-			return;
-		}
-		if (!code.data.startsWith(document.location.origin)) {
-			alert('Invalid QR Code');
-		}
-		stopStream();
-		goto(code.data.substring(document.location.origin.length));
-	}
+	import Scanner from '$lib/Scanner.svelte';
 </script>
 
 <Hero>
@@ -82,12 +11,8 @@
 		<!-- Modal header -->
 		<h3 class="text-xl font-semibold text-center pt-4 pb-3">Scan QR Code</h3>
 		<!-- Modal body -->
-		<div class="p-6 space-y-6 stack">
-			<canvas bind:this={canvasElement} hidden />
-
-			{#if loading && !stopped}
-				<Spinner />
-			{/if}
+		<div class="px-5 stack">
+			<Scanner />
 		</div>
 		<!-- Modal footer -->
 		<div class="flex items-center rounded p-3">
