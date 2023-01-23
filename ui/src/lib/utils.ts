ff001f1406163b4985bb242ae84c8a68fca7fe0a
Dispa Louis <louis.dispa@insa-rouen.fr>
2023-01-23 17:55:29 +0100

    Default api url to window origin

--- ui/src/lib/utils.ts
+++ ui/src/lib/utils.ts
@@ -1,9 +1,8 @@
-import { dev } from '$app/environment';
 import { goto as svelteGoto } from '$app/navigation';
 
 export const env = {
 	BASE_HREF: import.meta.env.VITE_BASE_HREF || '',
-	API_URL: import.meta.env.VITE_API_URL || dev ? 'http://127.0.0.1:3000' : ''
+	API_URL: import.meta.env.VITE_API_URL || window.location.origin
 };
 
 export async function goto(path: string) {
