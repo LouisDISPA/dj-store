723be00fa16f7b5e05340120ea99e49bc16f2b1a
Dispa Louis <louis.dispa@insa-rouen.fr>
2023-04-05 00:03:40 +0200

    Better heart

--- ui/src/lib/components/MusicTile.svelte
+++ ui/src/lib/components/MusicTile.svelte
@@ -45,17 +45,23 @@
 </td>
 <td>
 	<div class="text-max text-sm opacity-50 truncate">{artist}</div>
-	<div class="text-max  font-bold truncate">{title}</div>
+	<div class="text-max font-bold truncate">{title}</div>
 </td>
 <td>
 	<div class="rating flex items-center">
 		{#if onVote}
-			<input
-				type="checkbox"
-				class="mask mask-heart bg-red-500 opacity-30 checked:opacity-80"
-				on:change={toogleVote}
-				checked={is_voted}
-			/>
+			<button class="btn btn-ghost btn-circle p-2 md:p-1 md:mr-4" on:click={toogleVote}>
+				<svg
+					viewBox="0 0 24 24"
+					class="stroke-white stroke-1 fill-transparent"
+					class:fill-white={is_voted}
+					class:opacity-70={!is_voted}
+				>
+					<path
+						d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+					/>
+				</svg>
+			</button>
 		{:else if votes !== undefined}
 			<p class="badge text-xl py-5 px-4 mr-4">
 				{votes}
